29bc5035268601b1c066acc3e9c7142fb76e738c
Danialchhapani <dchhapani@outlook.com>
2025-11-22 12:15:57 +0530

    Create 07a_average_age_students.sql

---
+++ scripts/07a_average_age_students.sql
@@ -0,0 +1,13 @@
+/*
+========================================================================
+Average Age of Students
+========================================================================
+
+Write a SQL query to calculate the average age from the survey_info table.
+
+*/
+
+SELECT
+	AVG(Age) AS Average_age
+FROM 
+	survey_info
